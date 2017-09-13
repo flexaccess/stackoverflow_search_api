fecae06cfebed13ad2954a0dfe5480aba63e4155
Mirka <flexaccess@hotmail.com>
2017-09-13 03:38:11 +0300

    Change logic controller and new inteface

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,11 +11,23 @@ class MainController < ApplicationController
 
 		# http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle=how%20do&site=stackoverflow&filter=!Su916jdNbrmxot7gxi/
 		@q = params[:q]
-		@query = URI.escape("http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle=#{@q}&site=stackoverflow&filter=!Su916jdNbmxtsVd5Yq")
-		
-		@result = Net::HTTP.get(URI.parse(@query))
-		@result = JSON.parse(@result)
-		
+
+		if @q && @q.empty? == false
+			@query = URI.escape("http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle=#{@q}&site=stackoverflow&filter=!Su916jdNbmxtsVd5Yq")
+			@result = Net::HTTP.get(URI.parse(@query))
+			@result = JSON.parse(@result)
+
+			unless @result['items'].empty?
+				@result = @result['items']
+			else
+				@result = []
+			end
+		else
+			@result = false
+		end
+
+
+
 
 	end
 end
